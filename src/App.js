14b9835b6c680c48940e640c3411dcafaba5d90d
Dina Berry <41597107+diberry@users.noreply.github.com>
2020-11-16 08:51:04 -0800

    initial commit (#1)

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import './App.css';
+import { computerVision, isConfigured as ComputerVisionIsConfigured} from './VisualAI';
+
+function App() {
+
+  const [fileSelected, setFileSelected] = useState(null);
+  const [analysis, setAnalysis] = useState(null);
+  const [processing, setProcessing] = useState(false);
+  
+  const handleChange = (e) => {
+    setFileSelected(e.target.value)
+  }
+  const onFileUrlEntered = (e) => {
+
+    // hold UI
+    setProcessing(true);
+    setAnalysis(null);
+
+    computerVision(fileSelected || null).then((item) => {
+      // reset state/form
+      setAnalysis(item);
+      setFileSelected("");
+      setProcessing(false);
+    });
+
+  };
+
+  // Display JSON data in readable format
+  const PrettyPrintJson = (data) => {
+    return (<div><pre>{JSON.stringify(data, null, 2)}</pre></div>);
+  }
+
+  const DisplayResults = () => {
+    return (
+      <div>
+        <h2>Computer Vision Analysis</h2>
+        <div><img src={analysis.URL} height="200" border="1" alt={(analysis.description && analysis.description.captions && analysis.description.captions[0].text ? analysis.description.captions[0].text : "can't find caption")} /></div>
+        {PrettyPrintJson(analysis)}
+      </div>
+    )
+  };
+  
+  const Analyze = () => {
+    return (
+    <div>
+      <h1>Analyze image</h1>
+      {!processing &&
+        <div>
+          <div>
+            <label>URL</label>
+            <input type="text" placeholder="Enter URL or leave empty for random image from collection" size="50" onChange={handleChange}></input>
+          </div>
+          <button onClick={onFileUrlEntered}>Analyze</button>
+        </div>
+      }
+      {processing && <div>Processing</div>}
+      <hr />
+      {analysis && DisplayResults()}
+      </div>
+    )
+  }
+  
+  const CantAnalyze = () => {
+    return (
+      <div>Key and/or endpoint not configured in ./VisualAI.js</div>
+    )
+  }
+  
+  function Render() {
+    const ready = ComputerVisionIsConfigured();
+    if (ready) {
+      return <Analyze />;
+    }
+    return <CantAnalyze />;
+  }
+
+  return (
+    <div>
+      {Render()}
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/VisualAI.js
@@ -0,0 +1,96 @@
+// Azure SDK client libraries
+import { ComputerVisionClient } from '@azure/cognitiveservices-computervision';
+import { ApiKeyCredentials } from '@azure/ms-rest-js';
+
+// List of sample images to use in demo
+import RandomImageUrl from './DefaultImages';
+
+// Authentication requirements
+const key = process.env.REACT_APP_COMPUTERVISIONKEY;
+const endpoint = process.env.REACT_APP_COMPUTERVISIONENDPOINT;
+
+console.log(`key = ${key}`)
+console.log(`endpoint = ${endpoint}`)
+
+// Cognitive service features
+const visualFeatures = [
+    "ImageType",
+    "Faces",
+    "Adult",
+    "Categories",
+    "Color",
+    "Tags",
+    "Description",
+    "Objects",
+    "Brands"
+];
+
+export const isConfigured = () => {
+    const result = (key && endpoint && (key.length > 0) && (endpoint.length > 0)) ? true : false;
+    console.log(`key = ${key}`)
+    console.log(`endpoint = ${endpoint}`)
+    console.log(`ComputerVision isConfigured = ${result}`)
+    return result;
+}
+
+// Computer Vision detected Printed Text
+const includesText = async (tags) => {
+    return tags.filter((el) => {
+        return el.name.toLowerCase() === "text";
+    });
+}
+// Computer Vision detected Handwriting
+const includesHandwriting = async (tags) => {
+    return tags.filter((el) => {
+        return el.name.toLowerCase() === "handwriting";
+    });
+}
+// Wait for text detection to succeed
+const wait = (timeout) => {
+    return new Promise(resolve => {
+        setTimeout(resolve, timeout);
+    });
+}
+
+// Analyze Image from URL
+export const computerVision = async (url) => {
+
+    // authenticate to Azure service
+    const computerVisionClient = new ComputerVisionClient(
+        new ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': key } }), endpoint);
+
+    // get image URL - entered in form or random from Default Images
+    const urlToAnalyze = url || RandomImageUrl();
+    
+    // analyze image
+    const analysis = await computerVisionClient.analyzeImage(urlToAnalyze, { visualFeatures });
+
+    // text detected - what does it say and where is it
+    if (includesText(analysis.tags) || includesHandwriting(analysis.tags)) {
+        analysis.text = await readTextFromURL(computerVisionClient, urlToAnalyze);
+    }
+
+    // all information about image
+    return { "URL": urlToAnalyze, ...analysis};
+}
+// analyze text in image
+const readTextFromURL = async (client, url) => {
+    
+    let result = await client.read(url);
+    let operationID = result.operationLocation.split('/').slice(-1)[0];
+
+    // Wait for read recognition to complete
+    // result.status is initially undefined, since it's the result of read
+    const start = Date.now();
+    console.log(`${start} -${result?.status} `);
+    
+    while (result.status !== "succeeded") {
+        await wait(500);
+        console.log(`${Date.now() - start} -${result?.status} `);
+        result = await client.getReadResult(operationID);
+    }
+    
+    // Return the first page of result. 
+    // Replace[0] with the desired page if this is a multi-page file such as .pdf or.tiff.
+    return result.analyzeResult; 
+}
